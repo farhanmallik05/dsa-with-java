88dc69c3989c58cfd8960b169f0a2e74e6c056ee
Farhan Mallik <farhanmallick2005@gmail.com>
2026-09-01 16:08:09 +0530

    inserting a element

---
+++ Arrays/insert.java
@@ -0,0 +1,29 @@
+package Arrays;
+import java.util.Scanner;
+
+public class insert {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = {1, 2, 3, 4, 5};
+        int n = arr.length;
+        int x = sc.nextInt();
+        int pos = sc.nextInt();
+
+        int[] newArr = new int[n + 1];
+
+        for (int i = 0; i < pos; i++) {
+            newArr[i] = arr[i];
+        }
+
+        newArr[pos] = x;
+
+        for (int i = pos; i < n; i++) {
+            newArr[i + 1] = arr[i];
+        }
+
+        System.out.println("Array after insertion:");
+        for (int i = 0; i < newArr.length; i++) {
+            System.out.print(newArr[i] + " ");
+        }
+    }
+}
